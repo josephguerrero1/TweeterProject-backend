62e7001b0129c56968cf3fc2d7cb4349f8a8e746
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-06-29 20:42:46 -0600

    Started and partially completed some app decorators. Fixed some errors I was running into.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dbhelpers
 import json
 import traceback
 import sys
+import secrets
 
 app = Flask(__name__)
 
@@ -99,8 +100,6 @@ def get_user_followers():
         user_followers_json = json.dumps(user_followers, default=str)
         return Response(user_followers_json, mimetype="application/json", status=200)
 
-# How can I create a loginToken in the database and send a randomized loginToken back to the user
-
 
 @app.post("/api/users")
 def post_user():
@@ -111,7 +110,7 @@ def post_user():
     birthdate = request.json['birthdate']
     imageUrl = request.json.get('imageUrl')
     bannerUrl = request.json.get('bannerUrl')
-    loginToken = rand(str)
+    loginToken = secrets.token_urlsafe(50)
 
     userId = dbhelpers.run_insert_statement("INSERT INTO user(email, username, password, bio, birthdate, imageUrl, bannerUrl) VALUES (?, ?, ?, ?, ?, ?, ?)",
                                             [email, username, password, bio, birthdate, imageUrl, bannerUrl])
@@ -124,14 +123,140 @@ def post_user():
         return Response(user_json, mimetype="application/json", status=201)
 
 
-# @app.post("/api/login")
-# @app.post("/api/follows")
-# @app.post("/api/tweets")
-# @app.post("/api/tweet-likes")
-# @app.post("/api/comments")
-# @app.post("/api/comment-likes")
+@app.post("/api/login")
+def user_login():
+    email = request.json['email']
+    password = request.json['password']
+
+    checkUser = dbhelpers.run_select_statement(
+        "SELECT id, email, username, bio, birthdate, imageUrl, bannerUrl FROM user WHERE email = ?, password = ?", [
+            email, password]
+    )
+
+    if(len(checkUser) == 1):
+        loginToken = secrets.token_urlsafe(50)
+        user_id = checkUser[0][0]
+        userId = dbhelpers.run_insert_statement("INSERT INTO user_session (user_id, loginToken) VALUES (?, ?)",
+                                                [user_id, loginToken])
+    else:
+        return "Login did not work"
+
+    if(userId == None):
+        return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+    else:
+        username = checkUser[0][2]
+        bio = checkUser[0][3]
+        birthdate = checkUser[0][4]
+        imageUrl = checkUser[0][5]
+        bannerUrl = checkUser[0][6]
+
+        logged_in_user = {"userId": userId, "email": email, "username": username, "bio": bio,
+                          "birthdate": birthdate, "loginToken": loginToken, "imageUrl": imageUrl, "bannerUrl": bannerUrl}
+        logged_in_user_json = json.dumps(logged_in_user, default=str)
+        return Response(logged_in_user_json, mimetype="application/json", status=201)
+
+
+@app.post("/api/follows")
+def follow_user():
+    loginToken = request.json['loginToken']
+    followId = request.json['followId']
+    # userId =
+
+    userId = dbhelpers.run_insert_statement(
+        "INSERT INTO follow (user_id, followed_id) VALUES (?, ?)"), [userId, followId]
+
+    if(userId == None):
+        return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+    else:
+        user = "User has been followed!"
+        user_json = json.dumps(user, default=str)
+        return Response(user_json, mimetype="application/json", status=204)
+
+
+@app.post("/api/tweets")
+def post_tweet():
+    loginToken = request.json['loginToken']
+    content = request.json['content']
+    imageUrl = request.json['imageUrl']
+
+    userId = dbhelpers.run_insert_statement(
+        "INSERT INTO tweet (content, imageUrl) VALUES (?, ?)"), [content, imageUrl]
+
+    if(userId == None):
+        return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+    else:
+
+        params = request.args['loginToken']
+
+        tweet_info = dbhelpers.run_select_statement(
+            "SELECT t.id, t.user_id, u.username, u.imageUrl, t.content, t.createdAt, t.tweetimage_url FROM user_session us INNER JOIN `user` u ON us.user_id=u.id INNER JOIN tweet t ON u.id=t.user_id WHERE us.loginToken = ?", [
+                params]
+        )
+        tweet_info_json = json.dumps(tweet_info, default=str)
+        return Response(tweet_info_json, mimetype="application/json", status=201)
+
+
+@app.post("/api/tweet-likes")
+def like_tweet():
+    loginToken = request.json['login_Token']
+    tweetId = request.json['tweetId']
+    # userId =
+
+    userId = dbhelpers.run_insert_statement(
+        "INSERT INTO tweet_like (user_id, tweet_id) VALUES (?, ?)", [
+            userId, tweetId]
+    )
+
+    if(userId == None):
+        return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+    else:
+        tweet_like = "Tweet has been liked!"
+        tweet_like_json = json.dumps(tweet_like, default=str)
+        return Response(tweet_like_json, mimetype="application/json", status=201)
+
+
+@app.post("/api/comments")
+def post_comment():
+    loginToken = request.json['loginToken']
+    tweetId = request.json['tweetId']
+    content = request.json['content']
+
+    userId = dbhelpers.run_insert_statement(
+        "INSERT INTO comment (tweet_id, content) VALUES (?, ?)", [
+            tweetId, content]
+    )
+
+    if(userId == None):
+        return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+    else:
+        comment = "Comment has been posted!"
+        comment_json = json.dumps(comment, default=str)
+        return Response(comment_json, mimetype="application/json", status=201)
+
+
+@app.post("/api/comment-likes")
+def like_comment():
+    loginToken = request.json['loginToken']
+    commentId = request.json['commentId']
+
+
 # @app.delete("/api/users")
-# @app.delete("/api/login")
+@app.delete("/api/login")
+def logout():
+    loginToken = request.json['loginToken']
+
+    rowcount = dbhelpers.run_delete_statement(
+        "DELETE FROM user_session WHERE loginToken = ?", [loginToken]
+    )
+
+    # if(rowcount == 1):
+    #     # success
+    # elif(rowcount == 0):
+    #     # loginToken did not exist
+    # elif(rowcount == None):
+    #     # database error
+
+
 # @app.delete("/api/follows")
 # @app.delete("/api/tweets")
 # @app.delete("/api/tweet-likes")
