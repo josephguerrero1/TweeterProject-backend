3c9ee15c06e7ead74fd70674e739bd8b04df15ae
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-07-10 07:54:57 -0600

    Finished all functions for the /api/login endpoint

--- login.py
+++ login.py
@@ -10,30 +10,32 @@ class Login:
     # User Login
 
     def user_login():
-        email = request.json['email']
+        email = request.json.get('email')
+        username = request.json.get('username')
         password = request.json['password']
 
-        checkUser = dbhelpers.run_select_statement(
+        user_info = dbhelpers.run_select_statement(
             "SELECT id, email, username, bio, birthdate, imageUrl, bannerUrl FROM user WHERE email = ?, password = ?", [
                 email, password]
         )
 
-        if(len(checkUser) == 1):
+        if(len(user_info) == 1):
             loginToken = secrets.token_urlsafe(50)
-            user_id = checkUser[0][0]
-            userId = dbhelpers.run_insert_statement("INSERT INTO user_session (user_id, loginToken) VALUES (?, ?)",
-                                                    [user_id, loginToken])
+            userId = user_info[0][0]
+            user_session_id = dbhelpers.run_insert_statement("INSERT INTO user_session us (user_id, loginToken) VALUES (?, ?)",
+                                                             [userId, loginToken])
         else:
-            return "Login did not work"
+            return Response("Failed to get user information", mimetype="text/plain", status=500)
 
-        if(userId == None):
-            return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
+        if(user_session_id == None):
+            return Response("Failed to login", mimetype="text/plain", status=500)
         else:
-            username = checkUser[0][2]
-            bio = checkUser[0][3]
-            birthdate = checkUser[0][4]
-            imageUrl = checkUser[0][5]
-            bannerUrl = checkUser[0][6]
+            email = user_info[0][1]
+            username = user_info[0][2]
+            bio = user_info[0][3]
+            birthdate = user_info[0][4]
+            imageUrl = user_info[0][5]
+            bannerUrl = user_info[0][6]
 
             logged_in_user = {"userId": userId, "email": email, "username": username, "bio": bio,
                               "birthdate": birthdate, "loginToken": loginToken, "imageUrl": imageUrl, "bannerUrl": bannerUrl}
@@ -45,13 +47,22 @@ class Login:
     def user_logout():
         loginToken = request.json['loginToken']
 
+        user_id = dbhelpers.run_select_statement(
+            "SELECT us.user_id FROM user_session us WHERE us.loginToken = ?", [
+                loginToken]
+        )
+
+        if(user_id == None):
+            return Response("Invalid Login Token", mimetype="text/plain", status=400)
+        else:
+            userId = user_id[0][0]
+
         rowcount = dbhelpers.run_delete_statement(
-            "DELETE FROM user_session WHERE loginToken = ?", [loginToken]
+            "DELETE FROM user_session WHERE loginToken = ? AND user_id = ?", [
+                loginToken, userId]
         )
 
         if(rowcount == 1):
             return Response(status=204)
-        elif(rowcount == 0):
-            return Response("Error, loginToken does not exist!", mimetype="text/plain", status=400)
         elif(rowcount == None):
-            return Response("Database Error", mimetype="text/plain", status=500)
+            return Response("Failed to logout", mimetype="text/plain", status=500)
