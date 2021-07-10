15952df9d0f1c231d50bbff4935600573f7cca85
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-07-10 07:34:58 -0600

    Finished all functions for the /api/follows endpoint

--- follows.py
+++ follows.py
@@ -12,13 +12,29 @@ class Follows:
         userId = request.args['userId']
 
         all_user_follows = dbhelpers.run_select_statement(
-            "SELECT f.followed_id, u.email, u.username, u.bio, u.birthdate, u.imageUrl, u.bannerUrl FROM `user` u INNER JOIN follow f ON u.id = f.followed_id WHERE f.user_id= ?", [userId])
+            "SELECT u.id, u.email, u.username, u.bio, u.birthdate, u.imageUrl, u.bannerUrl FROM `user` u INNER JOIN follow f ON u.id=f.followed_id WHERE f.user_id= ?", [userId])
 
-        if(user_follows == None):
+        empty_user_follow = []
+
+        for user_follow in all_user_follows:
+            userId = all_user_follows[0]
+            email = all_user_follows[1]
+            username = all_user_follows[2]
+            bio = all_user_follows[3]
+            birthdate = all_user_follows[4]
+            imageUrl = all_user_follows[5]
+            bannerUrl = all_user_follows[6]
+
+            user_follow = {"userId": userId, "email": email, "username": username, "bio": bio, "birthdate": birthdate, "imageUrl": imageUrl, "bannerUrl": bannerUrl
+                           }
+
+            empty_user_follow.append(user_follow)
+
+        if(all_user_follows == None):
             return Response("Failed to GET user follows", mimetype="text/plain", status=500)
         else:
-            user_follows_json = json.dumps(user_follows, default=str)
-            return Response(user_follows_json, mimetype="application/json", status=200)
+            all_user_follows_json = json.dumps(empty_user_follow, default=str)
+            return Response(all_user_follows_json, mimetype="application/json", status=200)
 
     # Follow User
 
@@ -32,7 +48,7 @@ class Follows:
         )
 
         if(user_id == None):
-            return Response("Invalid Login Token", mimetype="text/plain", status=500)
+            return Response("Invalid Login Token", mimetype="text/plain", status=400)
         else:
             userId = user_id[0][0]
 
@@ -42,9 +58,8 @@ class Follows:
         )
 
         if(follow_id == None):
-            return Response("Follow ID is invalid", mimetype="text/plain", status=500)
+            return Response("Failed to follow user", mimetype="text/plain", status=500)
         else:
-
             return Response(status=204)
 
     # Unfollow User
@@ -53,6 +68,22 @@ class Follows:
         loginToken = request.json['loginToken']
         followId = request.json['followId']
 
-        rowcount = dbhelpers.run_delete_statement(
-            "DELETE FROM follow WHERE "
+        user_id = dbhelpers.run_select_statement(
+            "SELECT us.user_id FROM user_session us WHERE us.loginToken = ?", [
+                loginToken]
         )
+
+        if(user_id == None):
+            return Response("Invalid Login Token", mimetype="text/plain", status=400)
+        else:
+            userId = user_id[0][0]
+
+        rowcount = dbhelpers.run_delete_statement(
+            "DELETE from follow f WHERE f.user_id = ? AND f.followed_id = ?", [
+                userId, followId]
+        )
+
+        if(rowcount == 1):
+            return Response(status=204)
+        elif(rowcount == None):
+            return Response("Failed to unfollow user", mimetype="text/plain", status=500)
